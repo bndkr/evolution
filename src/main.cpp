0f399c9d4086882bf292f2927b1dc754eb0defa2
Ben Decker <A02269549@aggies.usu.edu>
2023-05-26 00:24:25 -0600

    organize repo structure, learn valueable cmake lesson

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
 #include "Buffers.hpp"
 #include "Render.hpp"
 #include "color_console/color.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 #include <iostream>
