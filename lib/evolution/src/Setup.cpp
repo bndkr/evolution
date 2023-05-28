a8f87824b07e85c08965353b7110d3bf88428313
Ben Decker <A02269549@aggies.usu.edu>
2023-05-28 16:25:25 -0600

    remove unnecessary dependency of lib on imgui

--- lib/evolution/src/Setup.cpp
+++ lib/evolution/src/Setup.cpp
@@ -2,9 +2,9 @@
 
 #include "color_console/color.hpp"
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_opengl3.h"
-#include "imgui/imgui_impl_glfw.h"
+// #include "imgui/imgui.h"
+// #include "imgui/imgui_impl_opengl3.h"
+// #include "imgui/imgui_impl_glfw.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
